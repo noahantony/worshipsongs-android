78e97d7fceb69f5398251a5774ccd8be0decb1fa
Noah <noah.nayagam@mcruncher.com>
2017-03-24 16:22:47 +0800

    Added support to view the import service option

--- app/src/main/java/org/worshipsongs/fragment/ServiceListFragment.java
+++ app/src/main/java/org/worshipsongs/fragment/ServiceListFragment.java
@@ -8,8 +8,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
@@ -32,7 +34,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.worshipsongs.CommonConstants;
+import org.worshipsongs.activity.DatabaseSettingActivity;
 import org.worshipsongs.activity.ServiceSongListActivity;
+import org.worshipsongs.locator.IImportDatabaseLocator;
+import org.worshipsongs.locator.ImportDatabaseLocator;
 import org.worshipsongs.utils.CommonUtils;
 import org.worshipsongs.utils.PropertyUtils;
 import org.worshipsongs.worship.R;
@@ -43,8 +48,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+
+import static com.google.android.gms.location.LocationRequest.create;
+import static org.worshipsongs.worship.R.style.MyDialogTheme;
 
 /**
  * author  :Pitchumani, madasamy
@@ -52,6 +62,7 @@ import java.util.Properties;
  */
 public class ServiceListFragment extends Fragment
 {
+    private IImportDatabaseLocator importDatabaseLocator = new ImportDatabaseLocator();
     List<String> serviceNames = new ArrayList<String>();
     String serviceName;
     TextView serviceMsg;
@@ -61,6 +72,8 @@ public class ServiceListFragment extends Fragment
     private ListView serviceListView;
     private File serviceFile = null;
     private ArrayAdapter<String> adapter;
+    private Button defaultDatabaseButton;
+    private TextView resultTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -86,7 +99,7 @@ public class ServiceListFragment extends Fragment
                 View promptsView = layoutInflater.inflate(R.layout.delete_confirmation_dialog, null);
                 TextView deleteMsg = (TextView) promptsView.findViewById(R.id.deleteMsg);
                 deleteMsg.setText(R.string.message_delete_playlist);
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(), R.style.MyDialogTheme));
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(), MyDialogTheme));
                 alertDialogBuilder.setView(promptsView);
                 alertDialogBuilder.setCancelable(false).setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener()
                 {
@@ -180,8 +193,6 @@ public class ServiceListFragment extends Fragment
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
     {
         inflater.inflate(R.menu.add_service_fav, menu);
-
-
         super.onCreateOptionsMenu(menu, inflater);
     }
 
@@ -194,7 +205,54 @@ public class ServiceListFragment extends Fragment
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.addToList:
+                // EITHER CALL THE METHOD HERE OR DO THE FUNCTION DIRECTLY
+                System.out.println("Pressed");
+                showDatabaseTypeDialog();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class ImportDatabaseOnClickListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view)
+        {
+            showDatabaseTypeDialog();
+        }
+    }
+
+    private void showDatabaseTypeDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
+        builder.setTitle(getString(R.string.type_service));
+        builder.setItems(R.array.dataBaseTypes, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                //importDatabaseLocator.load(DatabaseSettingActivity.this, getStringObjectMap(which));
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.getListView().setSelector(android.R.color.darker_gray);
+        dialog.show();
+    }
+
+    @NonNull
+    private Map<String, Object> getStringObjectMap(int which)
+    {
+        Map<String, Object> objectMap = new HashMap<String, Object>();
+        objectMap.put(CommonConstants.INDEX_KEY, which);
+        objectMap.put(CommonConstants.TEXTVIEW_KEY, resultTextView);
+        objectMap.put(CommonConstants.REVERT_DATABASE_BUTTON_KEY, defaultDatabaseButton);
+        return objectMap;
     }
 
     @Override
